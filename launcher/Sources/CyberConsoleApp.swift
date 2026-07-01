9a4bc689ee2a09ecc4e134d271a3bbfa58c330dc
Yuvraj Singh <singh.yuvraj31@icloud.com>
2026-07-01 15:30:34 -0500

    launcher: bump appVersion to 1.5.0 (mod-manager release line)

--- launcher/Sources/CyberConsoleApp.swift
+++ launcher/Sources/CyberConsoleApp.swift
@@ -3,7 +3,7 @@ import AppKit
 import UniformTypeIdentifiers
 
 enum Const {
-    static let appVersion = "1.4.0"
+    static let appVersion = "1.5.0"
     static let supportedGameVersion = "2.3.1"
     static let defaultGame = "\(NSHomeDirectory())/Library/Application Support/Steam/steamapps/common/Cyberpunk 2077"
     // Files copied from the app's Resources into <game>/red4ext/ on install. config.ini is REQUIRED - RED4ext
